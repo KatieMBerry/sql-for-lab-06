e64b9e2277b8acc338a86e49c80be0a46dba1964
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-11-03 08:26:16 -0800

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,28 @@ require('./lib/client').connect();
 
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Helloooooo World!')
+})
+
+// app.get('/cocktails', (req, res) => {//listening for a GET request on /cocktails url => a defined end-point or route
+//   console.log('hello');
+//   const cocktails = [
+//     {
+//       name: 'gin-and-juicey'
+//     },
+//     {
+//       name: 'jack-and-coke'
+//     },
+//     {
+//       name: 'dirty-martini'
+//     }
+//   ];
+
+//   res.json(cocktails);//responds with some json
+// });
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
